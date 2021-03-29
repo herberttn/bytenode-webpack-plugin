a910201c1c3f9f101707902628b07673223a8965
Herbert Treis Neto <htnetooo@gmail.com>
2021-03-29 00:10:55 -0300

    chore(deps): add linter

---
+++ .eslintrc.js
@@ -0,0 +1,137 @@
+/* eslint-env node */
+
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    warnOnUnsupportedTypeScriptVersion: true,
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  root: true,
+  rules: commonRules(),
+  overrides: [
+    overrideForTypeScriptFiles(),
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': [
+        '.js',
+        '.ts',
+      ],
+    },
+  },
+};
+
+function commonRules() {
+  return {
+    'array-bracket-spacing': ['error', 'never'],
+    'brace-style': ['error', '1tbs'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'comma-spacing': 'error',
+    'comma-style': 'error',
+    'computed-property-spacing': ['error', 'never'],
+    'eol-last': ['error', 'always'],
+    'linebreak-style': ['error', 'unix'],
+    'no-multiple-empty-lines': ['error', {
+      max: 1,
+      maxBOF: 0,
+      maxEOF: 1,
+    }],
+    'no-tabs': 'error',
+    'no-use-before-define': 'off', // enhanced by @typescript-eslint
+    'nonblock-statement-body-position': ['error', 'beside'],
+    'object-curly-spacing': ['error', 'always'],
+    'prefer-const': 'error',
+    'prefer-object-spread': 'error',
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'quotes': ['error', 'single', {
+      allowTemplateLiterals: true,
+    }],
+    'require-await': 'off', // enhanced by @typescript-eslint
+    'rest-spread-spacing': ['error', 'never'],
+    'semi': 'off', // enhanced by @typescript-eslint
+    'semi-style': ['error', 'last'],
+    'sort-imports': ['error', {
+      ignoreCase: true,
+      ignoreDeclarationSort: true,
+      ignoreMemberSort: false,
+    }],
+    'sort-keys': ['error', 'asc', {
+      caseSensitive: false,
+      natural: true,
+    }],
+    'sort-vars': ['error', {
+      ignoreCase: true,
+    }],
+  };
+}
+
+function overrideForTypeScriptFiles() {
+  return {
+    files: '*.ts',
+    excludedFiles: '*.js',
+    extends: [
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    ],
+    rules: {
+      '@typescript-eslint/ban-ts-comment': ['off', {
+        minimumDescriptionLength: 20,
+        'ts-check': 'allow-with-description',
+        'ts-expect-error': 'allow-with-description',
+        'ts-ignore': false,
+        'ts-nocheck': 'allow-with-description',
+      }],
+      '@typescript-eslint/ban-types': 'error',
+      '@typescript-eslint/consistent-type-imports': ['error', {
+        disallowTypeAnnotations: true,
+        prefer: 'type-imports',
+      }],
+      '@typescript-eslint/explicit-function-return-type': ['error', {
+        allowConciseArrowFunctionExpressionsStartingWithVoid: false,
+        allowDirectConstAssertionInArrowFunctions: true,
+        allowExpressions: true,
+        allowHigherOrderFunctions: true,
+        allowTypedFunctionExpressions: true,
+      }],
+      '@typescript-eslint/explicit-member-accessibility': 'off',
+
+      // switching this off because it doesn't really understand type guards
+      // and it's less strict than @typescript-eslint/explicit-function-return-type anyway
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+      '@typescript-eslint/indent': ['error', 2],
+      '@typescript-eslint/member-delimiter-style': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-extra-non-null-assertion': 'error',
+      '@typescript-eslint/no-extra-semi': 'error',
+      '@typescript-eslint/no-floating-promises': ['error', {
+        ignoreIIFE: true,
+        ignoreVoid: true,
+      }],
+      '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/no-unsafe-call': 'error',
+      '@typescript-eslint/no-unsafe-member-access': 'error',
+      '@typescript-eslint/no-unsafe-return': 'error',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/prefer-as-const': 'error',
+      '@typescript-eslint/require-await': 'error',
+      '@typescript-eslint/restrict-template-expressions': 'error',
+      '@typescript-eslint/semi': ['error', 'always', {
+        omitLastInOneLineBlock: false,
+      }],
+    },
+  };
+}
