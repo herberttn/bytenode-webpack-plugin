c82fe0221882d19a3d30b8fc3bad229eafb8b07e
Herbert Treis Neto <htnetooo@gmail.com>
2021-03-29 22:13:03 -0300

    test: add object as entry test combinations

--- test/entry-object.test.ts
+++ test/entry-object.test.ts
@@ -2,14 +2,72 @@ import { runWebpack } from './runner';
 
 describe('entry as an object', () => {
 
-  test('should support one main entry', async () => {
+  test('should support one named entry', async () => {
     const assets = await runWebpack({
       entry: {
-        main: './input/first.js',
+        named: './input/first.js',
       },
     });
 
-    expect(assets).toStrictEqual(['main.js']);
+    expect(assets).toStrictEqual([
+      'named.compiled.jsc',
+      'named.js',
+    ]);
+  });
+
+  test('should support one named entry while naming the output', async () => {
+    const assets = await runWebpack({
+      entry: {
+        named: './input/first.js',
+      },
+      output: {
+        filename: 'index.js',
+      },
+    });
+
+    expect(assets).toStrictEqual([
+      'index.js',
+      'named.compiled.jsc',
+    ]);
+  });
+
+  test('should support more than one named entry', async () => {
+    const assets = await runWebpack({
+      entry: {
+        firstNamed: './input/first.js',
+        secondNamed: './input/second.js',
+        thirdNamed: './input/third.js',
+      },
+    });
+
+    expect(assets).toStrictEqual([
+      'firstNamed.compiled.jsc',
+      'firstNamed.js',
+      'secondNamed.compiled.jsc',
+      'secondNamed.js',
+      'thirdNamed.compiled.jsc',
+      'thirdNamed.js',
+    ]);
+  });
+
+  test('should support more than one named entry while naming the output', async () => {
+    const assets = await runWebpack({
+      entry: {
+        firstNamed: './input/first.js',
+        secondNamed: './input/second.js',
+        thirdNamed: './input/third.js',
+      },
+      output: {
+        filename: 'index.js',
+      },
+    });
+
+    expect(assets).toStrictEqual([
+      'firstNamed.compiled.jsc',
+      'index.js',
+      'secondNamed.compiled.jsc',
+      'thirdNamed.compiled.jsc',
+    ]);
   });
 
 });
