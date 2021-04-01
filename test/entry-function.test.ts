700d7e481bfd59e925eb884a523a1581449ad16a
Herbert Treis Neto <htnetooo@gmail.com>
2021-03-31 23:35:49 -0300

    test: entry as function

---
+++ test/entry-function.test.ts
@@ -0,0 +1,13 @@
+import { runWebpack } from './runner';
+
+describe('entry as a function', () => {
+
+  test('should not be supported', async () => {
+    const assets = runWebpack({
+      entry: () => './input/first.js',
+    });
+
+    await expect(assets).rejects.toThrow('Entry as a function is not supported as of yet.');
+  });
+
+});
