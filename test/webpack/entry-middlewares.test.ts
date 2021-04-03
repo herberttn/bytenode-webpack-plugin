4fef71076965e2eca92042f1836bcec429c792e6
Herbert Treis Neto <htnetooo@gmail.com>
2021-04-03 18:55:03 -0300

    test: reorganize middlewares tests

--- test/webpack/entry-middlewares.test.ts
+++ test/webpack/entry-middlewares.test.ts
@@ -2,112 +2,120 @@ import { runWebpack } from './runner';
 
 describe('entry middlewares', () => {
 
-  test('should be supported on entry arrays (middleware first)', async () => {
-    const assets = await runWebpack({
-      entry: ['webpack-hot-middleware/client', './fixtures/first.js'],
+  describe('should be supported on entry arrays', () => {
+    test('middleware first', async () => {
+      const assets = await runWebpack({
+        entry: ['webpack-hot-middleware/client', './fixtures/first.js'],
+      });
+
+      expect(assets).toStrictEqual([
+        'main.compiled.jsc',
+        'main.js',
+      ]);
     });
 
-    expect(assets).toStrictEqual([
-      'main.compiled.jsc',
-      'main.js',
-    ]);
+    test('middleware last', async () => {
+      const assets = await runWebpack({
+        entry: ['./fixtures/first.js', 'webpack-hot-middleware/client'],
+      });
+
+      expect(assets).toStrictEqual([
+        'main.compiled.jsc',
+        'main.js',
+      ]);
+    });
+
+    describe('and while naming the output', () => {
+      test('middleware first', async () => {
+        const assets = await runWebpack({
+          entry: ['webpack-hot-middleware/client', './fixtures/first.js'],
+          output: {
+            filename: 'named.js',
+          },
+        });
+
+        expect(assets).toStrictEqual([
+          'main.compiled.jsc',
+          'named.js',
+        ]);
+      });
+
+      test('middleware last', async () => {
+        const assets = await runWebpack({
+          entry: ['./fixtures/first.js', 'webpack-hot-middleware/client'],
+          output: {
+            filename: 'named.js',
+          },
+        });
+
+        expect(assets).toStrictEqual([
+          'main.compiled.jsc',
+          'named.js',
+        ]);
+      });
+    });
   });
 
-  test('should be supported on entry arrays (middleware last)', async () => {
-    const assets = await runWebpack({
-      entry: ['./fixtures/first.js', 'webpack-hot-middleware/client'],
+  describe('should be supported on entry object of arrays', () => {
+    test('middleware first', async () => {
+      const assets = await runWebpack({
+        entry: {
+          firstNamed: ['webpack-hot-middleware/client', './fixtures/first.js'],
+        },
+      });
+
+      expect(assets).toStrictEqual([
+        'firstNamed.compiled.jsc',
+        'firstNamed.js',
+      ]);
     });
 
-    expect(assets).toStrictEqual([
-      'main.compiled.jsc',
-      'main.js',
-    ]);
-  });
+    test('middleware last', async () => {
+      const assets = await runWebpack({
+        entry: {
+          firstNamed: ['./fixtures/first.js', 'webpack-hot-middleware/client'],
+        },
+      });
 
-  test('should be supported on entry arrays while naming the output (middleware first)', async () => {
-    const assets = await runWebpack({
-      entry: ['webpack-hot-middleware/client', './fixtures/first.js'],
-      output: {
-        filename: 'named.js',
-      },
+      expect(assets).toStrictEqual([
+        'firstNamed.compiled.jsc',
+        'firstNamed.js',
+      ]);
     });
 
-    expect(assets).toStrictEqual([
-      'main.compiled.jsc',
-      'named.js',
-    ]);
-  });
+    describe('and while naming the output', () => {
+      test('middleware first', async () => {
+        const assets = await runWebpack({
+          entry: {
+            firstNamed: ['webpack-hot-middleware/client', './fixtures/first.js'],
+          },
+          output: {
+            filename: 'named.js',
+          },
+        });
 
-  test('should be supported on entry arrays while naming the output (middleware last)', async () => {
-    const assets = await runWebpack({
-      entry: ['./fixtures/first.js', 'webpack-hot-middleware/client'],
-      output: {
-        filename: 'named.js',
-      },
+        expect(assets).toStrictEqual([
+          'firstNamed.compiled.jsc',
+          'named.js',
+        ]);
+      });
+
+      test('middleware last', async () => {
+        const assets = await runWebpack({
+          entry: {
+            firstNamed: ['./fixtures/first.js', 'webpack-hot-middleware/client'],
+          },
+          output: {
+            filename: 'named.js',
+          },
+        });
+
+        expect(assets).toStrictEqual([
+          'firstNamed.compiled.jsc',
+          'named.js',
+        ]);
+      });
     });
-
-    expect(assets).toStrictEqual([
-      'main.compiled.jsc',
-      'named.js',
-    ]);
-  });
-
-  test('should be supported on entry object of arrays (middleware first)', async () => {
-    const assets = await runWebpack({
-      entry: {
-        firstNamed: ['webpack-hot-middleware/client', './fixtures/first.js'],
-      },
-    });
-
-    expect(assets).toStrictEqual([
-      'firstNamed.compiled.jsc',
-      'firstNamed.js',
-    ]);
-  });
-
-  test('should be supported on entry object of arrays (middleware last)', async () => {
-    const assets = await runWebpack({
-      entry: {
-        firstNamed: ['./fixtures/first.js', 'webpack-hot-middleware/client'],
-      },
-    });
-
-    expect(assets).toStrictEqual([
-      'firstNamed.compiled.jsc',
-      'firstNamed.js',
-    ]);
-  });
-
-  test('should be supported on entry object of arrays while naming the output (middleware first)', async () => {
-    const assets = await runWebpack({
-      entry: {
-        firstNamed: ['webpack-hot-middleware/client', './fixtures/first.js'],
-      },
-      output: {
-        filename: 'named.js',
-      },
-    });
-
-    expect(assets).toStrictEqual([
-      'firstNamed.compiled.jsc',
-      'named.js',
-    ]);
-  });
-
-  test('should be supported on entry object of arrays while naming the output (middleware last)', async () => {
-    const assets = await runWebpack({
-      entry: {
-        firstNamed: ['./fixtures/first.js', 'webpack-hot-middleware/client'],
-      },
-      output: {
-        filename: 'named.js',
-      },
-    });
-
-    expect(assets).toStrictEqual([
-      'firstNamed.compiled.jsc',
-      'named.js',
-    ]);
   });
 
 });
